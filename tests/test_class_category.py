a414ce97ba4eb16a093bf3afda5aee69d26e0191
Arina <ilamanova.arina@gmail.com>
2024-10-10 09:47:43 +0300

    Исправила мелкие моменты

--- tests/test_class_category.py
+++ tests/test_class_category.py
@@ -1,12 +1,6 @@
-from unittest.mock import Mock
-
-
 def test_product_init(test_category):
     assert test_category.name == "Ягоды"
     assert test_category.description == "Арбуз - это тоже ягода"
     assert len(test_category.products) == 1
     assert test_category.category_count == 1
     assert test_category.product_count == 1
-    x = Mock(category=["<function test_product at 0x0000025FB55EE8E0>"])
-    test_category.products = x
-    assert test_category.products == x
