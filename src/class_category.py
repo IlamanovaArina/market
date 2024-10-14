0a9460a7e250a60652e23e1d8a5cfa6ba842bd7e
Arina <ilamanova.arina@gmail.com>
2024-10-14 19:59:07 +0300

    Хоть какой-то результат, идём дальше

--- src/class_category.py
+++ src/class_category.py
@@ -1,3 +1,8 @@
+from itertools import product
+
+from src.class_product import Product
+
+
 class Category:
     name: str
     description: str
@@ -5,9 +10,25 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description, product):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = []
         Category.category_count += 1
-        Category.product_count += len(products)
+        Category.product_count += len(product)
+
+    def add_product(self, product):
+        if isinstance(product, Product):
+            self.__products.append(product)
+
+    @property
+    def products(self):
+        return self.__products
+
+    @products.setter
+    def products(self):
+        self.__products.append(product)
+        self.product_count += 1
+
+
+
--- src/class_product.py
+++ src/class_product.py
@@ -7,5 +7,21 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, my_dict):
+        name, description, price, quantity = my_dict.values()
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
