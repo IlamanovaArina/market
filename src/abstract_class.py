e54b8880ef0c6ba437714c9cde8fd2474ece3763
Arina <ilamanova.arina@gmail.com>
2024-10-30 14:38:10 +0300

    После линтеров

--- src/abstract_class.py
+++ src/abstract_class.py
@@ -18,6 +18,5 @@ class MixinInfo:
         super().__repr__()
         print(repr(self))
 
-
     def __repr__(self):
         return f"{self.__class__.__name__}, ({self.name}, {self.description}, {self.price}, {self.quantity})"
--- src/classes_child.py
+++ src/classes_child.py
@@ -11,7 +11,7 @@ class Smartphone(Product):
         self.color = color
 
     def __add__(self, other):
-        if type(other) == Smartphone:
+        if type(other) is Smartphone:
             return super().__add__(other)
         else:
             raise TypeError
@@ -26,7 +26,7 @@ class LawnGrass(Product):
         self.color = color
 
     def __add__(self, other):
-        if type(other) == LawnGrass:
+        if type(other) is LawnGrass:
             return super().__add__(other)
         else:
             raise TypeError
